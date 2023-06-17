ffded1254e46a928e69bfa25796e8fce4bc72377
Wilfred Cheah <73457309+wilfredcsw@users.noreply.github.com>
2023-06-18 06:13:17 +0800

    Update web.php
    trying to change gap

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,33 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('schedule', ['as' => 'pages.schedule', 'uses' => 'App\Http\Controllers\PageController@schedule']);
     Route::get('payment', ['as' => 'pages.payment', 'uses' => 'App\Http\Controllers\PageController@payment']);
 
-    Route::get('inventory', ['as' => 'pages.inventory', 'uses' => 'App\Http\Controllers\InventoryController@index']);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    //inventory parts dont touch
+    Route::get('inventory', ['as' => 'pages.inventory', 'uses' => 'App\Http\Controllers\InventoryController@index']);
+    
     // Add the following route for storing a product
     Route::post('products', [App\Http\Controllers\InventoryController::class, 'store'])->name('products.store');
     Route::post('products', ['as' => 'products.store', 'uses' => 'App\Http\Controllers\inventorycontroller@store']);

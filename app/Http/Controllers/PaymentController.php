d31ae9e570dc5d8760de3ea1434d21c78c08b99d
Wilfred <wilfredcsw@gmail.com>
2023-06-19 17:08:24 +0800

    feature: add payment table

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Product;
 
 class PaymentController extends Controller
 {
     public function index()
     {
-        $payments = Payment::all();
-        return view('pages.payment', ['payments'=>$payments]);
+        // $payments = Payment::all();
+        // return view('pages.payment', ['payments'=>$payments]);
+        $products = Product::all();
+        return view('pages.payment', compact('products'));
     }
 
 }
